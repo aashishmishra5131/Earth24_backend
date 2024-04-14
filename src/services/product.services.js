e0d5e8b44123118e7b2d451719f9cb847819a586
AashishMishra <aashishmishra5131@gmail.com>
2024-04-15 01:25:54 +0530

    Controllers

---
+++ src/services/product.services.js
@@ -0,0 +1,135 @@
+const Category=require("../models/category.model.js");
+const Product=require("../models/product.model.js");
+
+async function createProduct(reqData){
+    let topLevel=await Category.findOne({name:reqData.topLevelCategory});
+
+    if(!topLevel){
+        topLevel=new Category({
+            name:reqData.topLevelCategory,
+            level:1
+        })
+    }
+    let secondLevel=await Category.findOne({
+        name:reqData.secondLevelCategory,
+        parentCategory:topLevel._id,
+    })
+
+    if(!secondLevel){
+        secondLevel=new Category({
+            name:reqData.secondLevelCategory,
+            parentcategory:topLevel._id,
+            level:2
+        })
+
+    }
+    let thirdLevel=await Category.findOne({
+        name:reqData.thirdLevelCategory,
+        parentCategory:secondLevel._id,
+    })
+    if(!thirdLevel){
+        thirdLevel=new Category({
+            name:reqData.thirdLevelCategory,
+            parentcategory:secondLevel._id,
+            level:3
+        })
+    }
+
+    const product=new Product({
+        title:reqData.title,
+        color:reqData.color,
+        description:reqData.description,
+        discountedPrice:reqData.discountedPrice,
+        discountPersent:reqData.discountPersent,
+        imageUrl:reqData.imageUrl,
+        brand:reqData.brand,
+        price:reqData.price,
+        size:reqData.size,
+        quantity:reqData.quantity,
+        category:thirdLevel._id,
+    })
+    return await product.save();
+}
+
+async function deleteProduct(productId){
+    const product =await findProductById(productId);
+    await Product.findByIdAndDelete(productId);
+    return "Product deleted Successfully";
+}
+
+async function updateProduct(productId,reqData){
+    
+    return await Product.findByIdAndUpdate(productId,reqData);
+}
+
+async function findProductById(id){
+    const product=await Product.findById(id).populate("category").exec();
+    if(!product){
+        throw new Error("Product not Found with id "+ id);
+    }
+    return product;
+}
+
+
+async function getAllProducts(reqQuery){
+    let {category,color,size,minPrice,maxPrice,minDiscount,sort,stock,pageNumber,pageSize}=reqQuery;
+    pageSize=pageSize||10;
+    let query=Product.find().populate("category");
+    if(category){
+        const existCategory=await Category.findOne({name:category});
+        if(existCategory){
+            query=query.where("category").equals(existCategory._id);
+        }
+        else{
+            return {content:[],currentPage:1,totalPages:0}
+        }
+    }
+    if(color){
+        const colorSet=new Set(color.split(",").map(color=>color.trim().toLowerCase()));
+        const colorRegex=colorSet.size>0?new RegExp([...colorSet].join("|"),"i"):null;
+        query=query.where("color").regex(colorRegex);
+    }
+    if(size){
+        const sizeSet=new Set(size);
+        query.query.where("size.name").in([...sizeSet]);
+    }
+    if(minPrice&& maxPrice){
+        query=query.where('discountedPrice').get(minPrice).lte(maxPrice);
+    }
+    if(minDiscount){
+        query=query.where('discountedPersent').gt(minDiscount);
+    }
+    if(stock){
+        if(stock=="in_stock"){
+           query=await query.where("quantity").gt(0) 
+        }
+        else if(stock=="out_of_stock"){
+            query=await query.where("quantity").gt(1); 
+         }
+    }
+    if(sort){
+        const sortDirection=sort==="price_high"?-1:1;
+        query=query.sort({discountedPrice:sortDirection})
+    }
+    const totalProducts=await Product.countDocuments(query);
+    const skip=(pageNumber-1)*pageSize;
+    query=query.skip(skip).limit(pageSize);
+
+    const products=await query.exec();
+    const totalPages=Math.ceil(totalProducts/pageSize);
+    return {content:products,currentPage:pageNumber,totalPages,}
+}
+
+async function createMultipleProducts(products){
+    for(let product of products){
+        await createProduct(product);
+    }
+}
+module.exports={
+    createProduct,
+    deleteProduct,
+    updateProduct,
+    getAllProducts,
+    findProductById,
+    createMultipleProducts
+}
---
+++ src/services/rating.services.js
@@ -0,0 +1,23 @@
+const Rating =require("../models/rating.model.js")
+const productService=require("../services/product.services.js");
+
+async function createRating(req,user){
+    const product =await productService.findProductById(req.productId);
+    const rating=new Rating({
+        product:product._id,
+        user:user._id,
+        rating:req.rating,
+        createdAt:new Date(),
+    })
+
+    return await rating.save();
+}
+
+async function getProductRatings(productId){
+    return await Rating.find({product:productId});
+}
+
+module.exports={
+    createRating,
+    getProductRatings
+}
