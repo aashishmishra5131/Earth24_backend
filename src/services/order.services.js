c872910c2ff014a71c70216abc012355a952c1a6
AashishMishra <aashishmishra5131@gmail.com>
2024-04-14 12:25:04 +0530

    Error resolve

--- src/services/order.services.js
+++ src/services/order.services.js
@@ -1,6 +1,7 @@
 const cartService=require("../services/cart.services.js")
 const Address=require("../models/addresses.model.js");
-
+const Order=require("../models/order.model.js");
+const { populate } = require("../models/user.model.js");
 
 async function createOrder(user,shippAddress){
     let address;
@@ -31,7 +32,7 @@ async function createOrder(user,shippAddress){
         const createdOrderItem=await orderItem.save();
         orderItems.push(createdOrderItem)
     }
-    const createdOrder=new Order({
+    const createOrder=new Order({
         user,
         orderItems,
         totalPrice:cart.totalPrice,
@@ -43,3 +44,81 @@ async function createOrder(user,shippAddress){
     const savedOrder=await createOrder.save();
     return savedOrder;
 }
+
+async function placeOrder(orderId){
+    const order=await findOrderById(orderId);
+
+    order.orderStatus="PLACED";
+    order.paymentDetails.status="COMPLETED";
+    return await order.save();
+}
+
+async function confirmedOrder(orderId){
+    const order=await findOrderById(orderId);
+
+    order.orderStatus="CONFIRMED";
+    return await order.save();
+}
+
+async function shipOrder(orderId){
+    const order=await findOrderById(orderId);
+
+    order.orderStatus="SHIPPED";
+    return await order.save();
+}
+
+async function deliverOrder(orderId){
+    const order=await findOrderById(orderId);
+
+    order.orderStatus="DELIEVERED";
+    return await order.save();
+}
+
+async function cancelledOrder(orderId){
+    const order=await findOrderById(orderId);
+
+    order.orderStatus="CANCELLED";
+    return await order.save();
+}
+
+async function findOrderById(orderId){
+     const order=await Order.findById(orderId)
+     .populate("user")
+     .populate({path:"orderItems",populate:{path:"product"}})
+     .populate("shippingAddress")
+
+     return order;
+}
+
+async function usersOrderHistory(userId){
+    try {
+        const orders=await Order.find({user:userId,orderStatus:"PLACED"})
+        .populate({path:"orderItems",populate:{path:"product"}}).lean()
+        return orders;
+    } catch (error) {
+        throw new Error(error.message)
+    }
+}
+
+async function getAllorders(){
+    return await Order.find()
+    .populate({path:"orderItems",populate:{path:"product"}}).lean()
+}
+
+async function deleteOrder(orderId){
+    const order=await findOrderById(orderId);
+    await Order.findByIdAndDelete(order._id)
+}
+
+module.exports={
+    createOrder,
+    placeOrder,
+    confirmedOrder,
+    cancelledOrder, 
+    deliverOrder,
+    usersOrderHistory, 
+    getAllorders, 
+    deleteOrder,
+    findOrderById,
+    shipOrder
+};
